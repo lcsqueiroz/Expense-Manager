d87f60885aca7c273fa68f473d6e096e7c02d1bc
Lucas Queiroz Vieira <lqvieira7@gmail.com>
2025-06-29 22:24:42 -0300

    Criando a classe Usuario

---
+++ MyApp/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyApp
+{
+    public class User
+    {
+        private string _nome;
+        private int _senha;
+
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O nome não pode ser vazio ou conter apenas espaços em branco.");
+                }
+                _nome = value;
+            }
+        }
+
+        public int Senha
+        {
+            get { return _senha; }
+            set
+            {
+               if (_senha == null)
+                {
+                    throw new ArgumentException("A senha não pode ser vazia.");
+                }
+                _senha = value;
+            }
+        }
+
+            
+    }
+}
