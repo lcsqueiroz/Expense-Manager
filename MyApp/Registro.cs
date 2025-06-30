4b99f704bbf5392a46ee73212e2f2c261b6c1838
Lucas Queiroz Vieira <lqvieira7@gmail.com>
2025-06-29 23:15:19 -0300

    Ajustando classe Registro e Usuario (POO Aplication)

---
+++ MyApp/Registro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Registro
+    {
+        public void Registrar(Usuario Usuario)
+        {
+            string nomeUsuario = Usuario.Nome;
+            string senhaUsuario= Usuario.Senha;
+
+            string jsonUsuario = JsonSerializer.Serialize(Usuario);
+
+        }
+    }
+}
---
+++ MyApp/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class Usuario
+    {
+        private string _nome;
+        private string _senha;
+
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public string Senha
+        {
+            get { return _senha; }
+            set { _senha = value; }
+        }
+    }
+}
